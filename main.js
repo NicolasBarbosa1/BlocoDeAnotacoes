5f0a4c5ebbe50decaf5fea72617689bde97e10bc
Nicolas <b.nicolas.b@hotmail.com>
2020-05-27 19:03:39 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow } = require('electron');
+
+
+function createWindow(){
+    let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+    });
+
+mainWindow.loadFile(__dirname + '/src/index.html');
+mainWindow.on('closed', function(){
+    mainWindow = null;
+})
+
+}
+
+app.on('ready', createWindow);
